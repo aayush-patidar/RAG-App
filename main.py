3e72e0d7a8501fdf21f41152c65fc99fa0ef1fe7
Aayush Patidar <patidaraayush053@gmail.com>
2026-06-30 00:57:08 +0530

    updating main
    Signed-off-by: Aayush Patidar <patidaraayush053@gmail.com>

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from dotenv import load_dotenv
 import uuid
 import os
 import datetime
-
+from data_loader import load_end_chunk_pdf,embed_texts
+from vector_db import qdrantStorage
+from custom_types import RAGChunksAndSrc,RAGQueryResult,RAGSearchResulr,RAGUpsertResult 
 
 load_dotenv()
 
@@ -25,7 +27,25 @@ app=FastAPI()
 
 )
 async def rag_ingest_pdf(ctx:inngest.Context):
-    return {"hello"}
+    def _load(ctx :inngest.Context)-> RAGChunksAndSrc:
+        pdf_path=ctx.event.data["pdf_path"]
+        source_id=ctx.event.data.get("source_id",pdf_path)
+        chunks=load_end_chunk_pdf(pdf_path)
+        return RAGChunksAndSrc(chunks=chunks,source_id=source_id)
+    
+    def _uppsert(chunks_and_src:RAGChunksAndSrc)->RAGUpsertResult:
+        chunks=chunks_and_src.chunks
+        source_id=chunks_and_src.source_id
+        vectors=embed_texts[chunks]
+        ids=[str(uuid.uuids(uuid.NAMESPACE_URL),name=f"{source_id}:{i}")for i in range(len(chunks))]
+        payloads=[{"source":source_id,"text":chunks[i]} for i in range(len(chunks))]
+        qdrantStorage().upsert(ids,vectors,payloads)
+        return RAGUpsertResult(inngested=len(chunks))
+
+
+    chunks_and_src=await ctx.step.run("load_and_chunk",lambda:_load(ctx),output_type=RAGChunksAndSrc)
+    inngested=await ctx.step.run("embed_and_upsert",lambda:_uppsert(chunks_and_src),output_type=RAGUpsertResult)
+    return inngested.model_dump()
 
     
 inngest.fast_api.serve(app,inngest_client,[rag_ingest_pdf])
